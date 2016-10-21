9ab1e132de3a3c8c9a5f826b97637bbb84104f89
Dan Sabadis <dan_sabadas@hotmail.com>
2016-10-21 09:44:00 +0300

    finished chapter 1. added static files caching. the main entry of the app is the systemjs.config.js

--- src/OpenGameListWebApp/Startup.cs
+++ src/OpenGameListWebApp/Startup.cs
@@ -33,8 +33,20 @@ namespace OpenGameListWebApp
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
+            // Serve static files (html, css, js, images & more). See also the following URL:
+            // https://docs.asp.net/en/latest/fundamentals/static-files.html for further reference.
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                OnPrepareResponse = context =>
+                {
+                    var headers = context.Context.Response.Headers;
+                    // Disable caching for all static files.
+                    headers["Cache-Control"] = Configuration["StaticFiles:Headers:Cache-Control"];
+                    headers["Pragma"] = Configuration["StaticFiles:Headers:Pragma"];
+                    headers["Expires"] = Configuration["StaticFiles:Headers:Expires"];
+                }
+            });
+            //app.UseStaticFiles();
             app.UseMvc();
         }
     }
