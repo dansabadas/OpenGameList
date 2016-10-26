dcb87abec3885daea92d3aa69920f38cf2c12d39
dansab <dan.sabadas@gmail.com>
2016-10-26 20:07:54 +0300

    arrived at 215 splitting the itemdetailview

--- src/OpenGameListWebApp/Scripts/app/item-detail.component.ts
+++ src/OpenGameListWebApp/Scripts/app/item-detail.component.ts
@@ -11,13 +11,22 @@ import {ItemService} from "./item.service";
           <ul>
               <li>
                   <label>Title:</label>
-                  <input [ngModel]="item.Title" placeholder="Insert the title..."/>
+                  <input [(ngModel)]="item.Title" placeholder="Insert the title..."/>
               </li>
               <li>
                   <label>Description:</label>
                   <textarea [(ngModel)]="item.Description" placeholder="Insert a suitable description..."></textarea>
               </li>
           </ul>
+          <div *ngIf="item.Id == 0" class="commands insert">
+              <input type="button" value="Save" (click)="onInsert(item)" />
+              <input type="button" value="Cancel" (click)="onBack()" />
+          </div>
+          <div *ngIf="item.Id != 0" class="commands update">
+              <input type="button" value="Update" (click)="onUpdate(item)" />
+              <input type="button" value="Delete" (click)="onDelete(item)" />
+              <input type="button" value="Back" (click)="onBack()" />
+          </div>
         </div>
     `,//[ngModel] - means only one-way communication, banana-in-a-box [(ngModel)] means 2-way comm
     styles: [`
@@ -45,13 +54,56 @@ export class ItemDetailComponent {
     }
 
     ngOnInit() {
-        var id = +this.activatedRoute.snapshot.params['id'];
-        if (id) {
-            this.itemService.get(id).subscribe(item => this.item = item);
-            console.log(this.item);
-        } else {
-            console.log("Invalid id: routing back home...");
-            this.router.navigate([""]);
-        }
+      var id = +this.activatedRoute.snapshot.params["id"];
+      if (id) {
+        this.itemService.get(id).subscribe(
+          item => this.item = item
+        );
+      }
+      else if (id === 0) {
+        console.log("id is 0: adding a new item...");
+        this.item = new Item(0, "New Item", null);
+      }
+      else {
+        console.log("Invalid id: routing back to home...");
+        this.router.navigate([""]);
+      }
+    }
+
+    onInsert(item: Item) {
+      this.itemService.add(item).subscribe(
+        (data) => {
+          this.item = data;
+          console.log("Item " + this.item.Id + " has been added.");
+          this.router.navigate([""]);
+        },
+        (error) => console.log(error)
+      );
+    }
+
+    onUpdate(item: Item) {
+      this.itemService.update(item).subscribe(
+        (data) => {
+          this.item = data;
+          console.log("Item " + this.item.Id + " has been updated.");
+          this.router.navigate([""]);
+        },
+        (error) => console.log(error)
+      );
+    }
+
+    onDelete(item: Item) {
+      var id = item.Id;
+      this.itemService.delete(id).subscribe(
+        (data) => {
+          console.log("Item " + id + " has been deleted.");
+          this.router.navigate([""]);
+        },
+        (error) => console.log(error)
+      );
+    }
+
+    onBack() {
+      this.router.navigate([""]);
     }
 }
