6bb7cb7aac19fa0c714418b1ff04e0b95bb9aece
Dan Sabadis <dan_sabadas@hotmail.com>
2016-11-04 16:45:38 +0200

    arrived at 313 adding middleware jwtprovider to the pipeline

--- src/OpenGameListWebApp/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ src/OpenGameListWebApp/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -2,8 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
-using OpenGameListWebApp.Data;
 
 namespace OpenGameListWebApp.Data.Migrations
 {
