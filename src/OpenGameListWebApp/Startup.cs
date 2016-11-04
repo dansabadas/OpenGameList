e898c6e7c04b58f3841c47b3a7bfe61a8fefab88
Dan Sabadis <dan_sabadas@hotmail.com>
2016-11-04 16:51:31 +0200

    arrived at Angular 2 login form

--- src/OpenGameListWebApp/Startup.cs
+++ src/OpenGameListWebApp/Startup.cs
@@ -11,85 +11,108 @@ using OpenGameListWebApp.Data.Items;
 using OpenGameListWebApp.ViewModels;
 using System;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using OpenGameListWebApp.Classes;
 using OpenGameListWebApp.Data.Users;
 
 namespace OpenGameListWebApp
 {
-  public class Startup
-  {
-    public Startup(IHostingEnvironment env)
+    public class Startup
     {
-      var builder = new ConfigurationBuilder()
-          .SetBasePath(env.ContentRootPath)
-          .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-          .AddEnvironmentVariables();
-      Configuration = builder.Build();
-    }
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
 
-    public IConfigurationRoot Configuration { get; }
+        public IConfigurationRoot Configuration { get; }
 
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-        // Add framework services.
-        services.AddMvc();
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
 
-        // Add EntityFramework's Identity support.
-        services.AddEntityFramework();
+            // Add EntityFramework's Identity support.
+            services.AddEntityFramework();
 
-        // Add Identity Services & Stores
-        services
-            .AddIdentity<ApplicationUser, IdentityRole>(config => {
-                config.User.RequireUniqueEmail = true;
-                config.Password.RequireNonAlphanumeric = false;
-                config.Cookies.ApplicationCookie.AutomaticChallenge = false;
-            })
-            .AddEntityFrameworkStores<ApplicationDbContext>()
-            .AddDefaultTokenProviders();
+            // Add Identity Services & Stores
+            services
+                .AddIdentity<ApplicationUser, IdentityRole>(config =>
+                {
+                    config.User.RequireUniqueEmail = true;
+                    config.Password.RequireNonAlphanumeric = false;
+                    config.Cookies.ApplicationCookie.AutomaticChallenge = false;
+                })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
 
             //// Add ApplicationDbContext.
             services.AddDbContext<ApplicationDbContext>(options =>
-          options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
+                    options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
 
-      // Add ApplicationDbContext's DbSeeder
-      services.AddSingleton<DbSeeder>();
-    }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, DbSeeder dbSeeder)
-    {
-      loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-      loggerFactory.AddDebug();
-
-      // Serve static files (html, css, js, images & more). See also the following URL:
-      // https://docs.asp.net/en/latest/fundamentals/static-files.html for further reference.
-      app.UseStaticFiles(new StaticFileOptions
-      {
-        OnPrepareResponse = context =>
-        {
-          var headers = context.Context.Response.Headers;
-                  // Disable caching for all static files.
-                  headers["Cache-Control"] = Configuration["StaticFiles:Headers:Cache-Control"];
-          headers["Pragma"] = Configuration["StaticFiles:Headers:Pragma"];
-          headers["Expires"] = Configuration["StaticFiles:Headers:Expires"];
+            // Add ApplicationDbContext's DbSeeder
+            services.AddSingleton<DbSeeder>();
         }
-      });
-      //app.UseStaticFiles();
-      app.UseMvc();
 
-      // TinyMapper binding configuration
-      TinyMapper.Bind<Item, ItemViewModel>();
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, DbSeeder dbSeeder)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
 
-      // Seed the Database (if needed)
-      try
-      {
-        dbSeeder.SeedAsync().Wait();
-      }
-      catch (AggregateException e)
-      {
-        throw new Exception(e.ToString());
-      }
+            // Serve static files (html, css, js, images & more). See also the following URL:
+            // https://docs.asp.net/en/latest/fundamentals/static-files.html for further reference.
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                OnPrepareResponse = context =>
+                {
+                    var headers = context.Context.Response.Headers;
+                    // Disable caching for all static files.
+                    headers["Cache-Control"] = Configuration["StaticFiles:Headers:Cache-Control"];
+                    headers["Pragma"] = Configuration["StaticFiles:Headers:Pragma"];
+                    headers["Expires"] = Configuration["StaticFiles:Headers:Expires"];
+                }
+            });
+
+            // Add a custom Jwt Provider to generate Tokens
+            app.UseJwtProvider();
+
+            // Add the Jwt Bearer Header Authentication to validate Tokens
+            app.UseJwtBearerAuthentication(new JwtBearerOptions
+            {
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true,
+                RequireHttpsMetadata = false,
+                TokenValidationParameters = new TokenValidationParameters
+                {
+                    IssuerSigningKey = JwtProvider.SecurityKey,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = JwtProvider.Issuer,
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                }
+            });
+
+            //app.UseStaticFiles();
+            app.UseMvc();
+
+            // TinyMapper binding configuration
+            TinyMapper.Bind<Item, ItemViewModel>();
+
+            // Seed the Database (if needed)
+            try
+            {
+                dbSeeder.SeedAsync().Wait();
+            }
+            catch (AggregateException e)
+            {
+                throw new Exception(e.ToString());
+            }
+        }
     }
-  }
 }
